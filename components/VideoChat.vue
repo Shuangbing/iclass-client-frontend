eaf0894aba0bcfe6791876b90e465d50bc74f764
Shuangbing <he.shuangbing@gmail.com>
2020-10-21 21:37:50 +0900

    add firebase config & skyway api

--- components/VideoChat.vue
+++ components/VideoChat.vue
@@ -22,7 +22,7 @@
                   @click="cameraOnOrOff"
                 />
                 <a-button
-                  :type="micphoneOn ? 'primary' : 'danger'"
+                  type="primary"
                   shape="circle"
                   icon="sound"
                   @click="micphoneOnOrOff"
@@ -32,80 +32,35 @@
           </a-card>
         </div>
       </a-col>
-      <a-col :span="8">
+      <a-col
+        :span="8"
+        id="remote-streams"
+        v-for="remoteStream in remoteStreams"
+        :ref="remoteStream.peerId"
+        :key="remoteStream.peerId"
+      >
         <div class="video-chat-view">
           <a-card hoverable>
             <video
               slot="cover"
-              ref="video1"
-              id="video1"
+              :ref="`video-remote-${remoteStream.peerId}`"
               width="100%"
               height="100%"
               autoplay
-              muted
               object-fit="contain"
+              :srcObject.prop="remoteStream"
             ></video>
-            <a-card-meta title="User2">
+            <a-card-meta :title="remoteStream.peerId">
               <div slot="description">
                 <a-button
-                  :type="micphoneOn ? 'primary' : 'danger'"
+                  :type="
+                    remoteStreamsMute[remoteStream.peerId]
+                      ? 'danger'
+                      : 'primary'
+                  "
                   shape="circle"
                   icon="sound"
-                  @click="micphoneOnOrOff"
-                />
-              </div>
-            </a-card-meta>
-          </a-card>
-        </div>
-      </a-col>
-      <a-col :span="8">
-        <div class="video-chat-view">
-          <a-card hoverable>
-            <video
-              slot="cover"
-              ref="video2"
-              id="video2"
-              width="100%"
-              height="100%"
-              autoplay
-              muted
-              object-fit="contain"
-            ></video>
-            <a-card-meta title="User3">
-              <div slot="description">
-                <a-button
-                  :type="micphoneOn ? 'primary' : 'danger'"
-                  shape="circle"
-                  icon="sound"
-                  @click="micphoneOnOrOff"
-                />
-              </div>
-            </a-card-meta>
-          </a-card>
-        </div>
-      </a-col>
-    </a-row>
-    <a-row :gutter="[16, 8]">
-      <a-col :span="8">
-        <div class="video-chat-view">
-          <a-card hoverable>
-            <video
-              slot="cover"
-              ref="video2"
-              id="video2"
-              width="100%"
-              height="100%"
-              autoplay
-              muted
-              object-fit="contain"
-            ></video>
-            <a-card-meta title="User3">
-              <div slot="description">
-                <a-button
-                  :type="micphoneOn ? 'primary' : 'danger'"
-                  shape="circle"
-                  icon="sound"
-                  @click="micphoneOnOrOff"
+                  @click="micphoneRemote(remoteStream.peerId)"
                 />
               </div>
             </a-card-meta>
@@ -117,31 +72,73 @@
 </template>
 
 <script>
+import Peer, { SfuRoom } from "skyway-js";
+
 export default {
   props: {
     groupData: Object,
   },
   data() {
     return {
+      peer: "GroupChat",
       cameraOn: true,
       micphoneOn: true,
       video: {},
       videoStream: {},
+      room: null,
+      localStream: undefined,
+      remoteStreams: [],
+      remoteStreamsMute: {},
     };
   },
-  mounted() {
+  async mounted() {
     this.video = this.$refs.video;
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices
+      await navigator.mediaDevices
         .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
+        .then(async (stream) => {
           this.videoStream = stream;
           this.video.srcObject = this.videoStream;
-          this.video.play();
+          await this.video.play();
         });
     }
+
+    this.peer = await new Peer(this.groupData.user.userId, {
+      key: process.env.SKYWAY_API_KEY,
+    });
+    this.peer.on("open", this.connect);
+  },
+  async beforeDestroy() {
+    this.room.close();
   },
   methods: {
+    connect() {
+      if (!this.peer || !this.peer.open) {
+        return;
+      }
+
+      this.room = this.peer.joinRoom(this.groupData.group.groupCode, {
+        mode: "sfu",
+        stream: this.videoStream,
+      });
+
+      if (this.room) {
+        this.room.on("stream", (stream) => {
+          this.remoteStreamsMute[stream.peerId] = false;
+          this.remoteStreams.push(stream);
+        });
+
+        this.room.on("peerLeave", (peerId) => {
+          this.remoteStreams = this.remoteStreams.filter(
+            (stream) => stream.peerId != peerId
+          );
+          const audio = document.getElementById(peerId);
+          if (audio) {
+            audio.remove();
+          }
+        });
+      }
+    },
     cameraOnOrOff() {
       if (this.cameraOn) {
         this.cameraOn = false;
@@ -156,6 +153,14 @@ export default {
         this.micphoneOn = true;
       }
     },
+    micphoneRemote(peerId) {
+      const remoteVideoMute = this.$refs[`video-remote-${peerId}`][0];
+      if (remoteVideoMute.muted) {
+        remoteVideoMute.muted = false;
+      } else {
+        remoteVideoMute.muted = true;
+      }
+    },
   },
 };
 </script>
@@ -163,8 +168,6 @@ export default {
 <style>
 .video-chat-view {
   min-height: 20vh;
-}
-.video-chat-view .ant-card {
 }
 .video-chat-view video {
   object-fit: cover;
