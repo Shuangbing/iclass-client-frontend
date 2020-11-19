22223896b4a1b4559c788b8729553b4327d1e33a
Shuangbing <he.shuangbing@gmail.com>
2020-11-19 18:36:46 +0900

    add google form redirect

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -3,12 +3,20 @@
     <template #icon>
       <a-icon type="smile" theme="twoTone" />
     </template>
+    <template #extra>
+      <a-button type="primary" @click="redirectToAsk"> アンケート </a-button>
+    </template>
   </a-result>
 </template>
 <script>
 export default {
   data() {
     return {};
+  },
+  methods: {
+    redirectToAsk() {
+      location.href = "https://forms.gle/msN7VFSt5J947eNw9";
+    },
   },
 };
 </script>
